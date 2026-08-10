aecaf4c15c6f0d0dea6ab8d8d896229e63e3eaec
Usama El-Sharkawi <usamasharkawi@gmail.com>
2026-08-11 00:07:53 +0300

    Add TypeScript as dev dependency and update package.json scripts

---
+++ next-gen-01-starting-setup/src/app.ts
@@ -0,0 +1,46 @@
+// Code goes here!
+
+// const myName = "usama";
+// myName = "max" //error
+
+// >>with var you have a globle or a functional scope
+//>>with let is block scope 
+
+// var sum;
+
+// function add(a:number,b:number){
+//     sum = a+ b;
+//     return sum
+// }
+
+// console.log(sum)
+
+
+
+// if(true){
+// var x = 1
+// }
+
+// console.log(x)
+
+
+// if(true){
+// let y = 1
+// }
+
+// console.log(y) //can not find y in the globle scope 
+
+
+
+//==============================================
+
+// Arrow functions
+
+const add = (a:number , b:number = 2)=>{
+    return a + b;
+}
+
+console.log(add(10,6))
+console.log(add(10))
+
+cont  numbers:[1,-1]
